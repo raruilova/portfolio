9e4e26c24794b95895c17c892a3f3babfe09c239
robinson <robinjosellove@gmail.com>
2023-06-21 00:48:04 -0500

    finished

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,28 @@
 "use client";
-import { Header } from '@/components/Header'
-import { Nabvar } from '@/components/Navbar';
-import '@/styles/globals.css';
-import { ThemeProvider } from 'next-themes';
-import { Inter } from 'next/font/google'
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { Nabvar } from "@/components/Navbar";
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <Header />
       <body className={inter.className}>
-        <ThemeProvider enableSystem={true} attribute='class'>
+        <ThemeProvider enableSystem={true} attribute="class">
           <Nabvar />
           {children}
+          <Footer />
         </ThemeProvider>
-        
-        </body>
+      </body>
     </html>
-  )
+  );
 }
