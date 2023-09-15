fab95e527dde06c3e90a9454b91bfe3683843e78
robinson <robinjosellove@gmail.com>
2023-09-14 21:22:59 -0500

    fix

--- src/context/ProjectsContext.ts
+++ src/context/ProjectsContext.ts
@@ -1,7 +1,5 @@
 "use client";
-import { ProjectImage } from "@/interfaces/projectImage";
 import { Projects } from "@/interfaces/projects";
-import { ProjectResponse } from "@/interfaces/projectsResponse";
 import { createContext } from "react";
 
 interface ProjectsContextProps {
